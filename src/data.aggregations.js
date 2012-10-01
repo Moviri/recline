bd125e956cb009a367828b54a0033ad038fbda49
Mario Scriminaci <mario.scriminaci@moviri.com>
2012-10-01 17:25:21 +0200

    add insersection function

--- src/data.aggregations.js
+++ src/data.aggregations.js
@@ -17,5 +17,44 @@ this.recline.Data = this.recline.Data || {};
 		
 	};
 	
+	var myIsEqual = function (object, other, key) {
+
+        var spl = key.split('.'),
+            val1, val2;
+
+        if (spl.length > 0) {
+            val1 = object;
+            val2 = other;
+
+            for (var k = 0; k < spl.length; k++) {
+                arr = spl[k].match(/(.*)\[\'?(\d*\w*)\'?\]/i);
+                if (arr && arr.length == 3) {
+                    val1 = (val1[arr[1]]) ? val1[arr[1]][arr[2]] : val1[spl[k]];
+                    val2 = (val2[arr[1]]) ? val2[arr[1]][arr[2]] : val2[spl[k]];
+                } else {
+                    val1 = val1[spl[k]];
+                    val2 = val2[spl[k]];
+                }
+            }
+        }
+        return _.isEqual(val1, val2);
+    };
+
+    my.Aggregations.intersectionObjects = my.Aggregations.intersectObjects = function (key, array) {
+        var slice = Array.prototype.slice;
+        // added this line as a utility
+        var rest = slice.call(arguments, 1);
+        return _.filter(_.uniq(array), function (item) {
+            return _.every(rest, function (other) {
+                //return _.indexOf(other, item) >= 0;
+                return _.any(other, function (element) {
+                    var control = myIsEqual(element, item, key);
+                    if (control) _.extend(item, element);
+                    return control;
+                });
+            });
+        });
+    };
+	
 	
 })(this.recline.Data);
