09afbb978807ed18c23c76820bc633bf53f3acf0
Davide Malagoli <davide.malagoli@moviri.com>
2013-01-11 16:16:19 +0100

    fix on composed views

--- src/extensions/model/model.extensions.facets.js
+++ src/extensions/model/model.extensions.facets.js
@@ -10,4 +10,26 @@
 
     });
 
+
+
+
+
+
+
 }(jQuery));
+
+recline.Model.Query.prototype = $.extend(recline.Model.Query.prototype, {
+    addFacetNoEvent:function (fieldId) {
+        var facets = this.get('facets');
+        // Assume id and fieldId should be the same (TODO: this need not be true if we want to add two different type of facets on same field)
+        if (_.contains(_.keys(facets), fieldId)) {
+            return;
+        }
+        facets[fieldId] = {
+            terms:{ field:fieldId }
+        };
+        this.set({facets:facets}, {silent:true});
+
+    }
+
+});
