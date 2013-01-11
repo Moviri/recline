36ed854e103de9e53e42675b1a0dc806616dbb29
giobaldac <giorgio.baldaccini@moviri.com>
2013-01-11 16:27:56 +0100

    MODA-19 bis

--- src/extensions/views/view.no_data.js
+++ src/extensions/views/view.no_data.js
@@ -5,11 +5,17 @@ this.recline.View = this.recline.View || {};
     "use strict";
 
     view.NoDataMsg = Backbone.View.extend({
-    	template:"<div class='noData' style='display:table;width:100%;height:100%;border:1px dotted lightgrey;font-size:18px;'><p style='display:table-cell;height:100%;margin-left: auto;margin-right: auto;text-align: center;margin-bottom: auto;margin-top: auto;vertical-align: middle;'>No Data Available!</p></div>",
+    	templateP1:"<div class='noData' style='display:table;width:100%;height:100%;border:1px dotted lightgrey;font-size:18px;'>" +
+    			"<p style='display:table-cell;height:100%;margin-left: auto;margin-right: auto;text-align: center;margin-bottom: auto;margin-top: auto;vertical-align: middle;'>",
+    	_internalMsg : "No Data Available!",
+    	templateP2:"</p></div>",
         initialize:function() {
         },
-        create:function() {
-        	return this.template;
+        create:function(msg) {
+        	if (msg)
+        		return this.templateP1+msg+this.templateP2;
+        	
+        	return this.templateP1+this._internalMsg+this.templateP2
         }
     });
 })(jQuery, recline.View);
