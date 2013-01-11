0287939608abee96f058732f84fefc1d2b3301db
giobaldac <giorgio.baldaccini@moviri.com>
2013-01-11 15:29:13 +0100

    Added noData view

---
+++ src/extensions/views/view.no_data.js
@@ -0,0 +1,16 @@
+this.recline = this.recline || {};
+this.recline.View = this.recline.View || {};
+
+(function ($, view) {
+    "use strict";
+
+    view.NoDataMsg = Backbone.View.extend({
+    	template:"<div class='noData' style='display:table;width:100%;height:100%;border:1px dotted lightgrey;font-size:18px;'><p style='display:table-cell;height:100%;margin-left: auto;margin-right: auto;text-align: center;margin-bottom: auto;margin-top: auto;vertical-align: middle;'>No Data Available!</p></div>",
+
+        initialize:function() {
+        },
+        create:function() {
+        	return this.template;
+        }
+    });
+})(jQuery, recline.View);
