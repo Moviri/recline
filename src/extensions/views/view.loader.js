9bdf416d0748ca3e7092dc5d2d3d6b7dfb2bc432
giobaldac <giorgio.baldaccini@moviri.com>
2013-02-07 11:22:07 +0100

    MODA-135

--- src/extensions/views/view.loader.js
+++ src/extensions/views/view.loader.js
@@ -20,6 +20,7 @@ this.recline.View = this.recline.View || {};
         },
         render:function () {
         	$(document.body).append(this.htmlLoader.replace("{{baseurl}}", this.baseurl));
+        	this.divOver.show();
         	this.bindDatasets(this.datasets);
         	this.bindCharts(this.charts);
         },
@@ -33,14 +34,14 @@ this.recline.View = this.recline.View || {};
     		</div>',
     	incLoaderCount : function() {
     		this.loaderCount++;
-    		//console.log("Start task - loaderCount = "+this.loaderCount)
+    		console.log("Start task - loaderCount = "+this.loaderCount)
     		this.divOver.show();
     		document.getElementById("loadingImage").style.display = "block"; 
     	},
     	decLoaderCount : function() { 
     		var self = this;
     		this.loaderCount--;
-    		//console.log("End task - loaderCount = "+this.loaderCount)
+    		console.log("End task - loaderCount = "+this.loaderCount)
     		if (this.loaderCount <= 0) {
     			//setTimeout(function() {
     				document.getElementById("loadingImage").style.display = "none";
