bbb236c3b60e3e9f1647300017af586953625b10
giobaldac <giorgio.baldaccini@moviri.com>
2013-03-11 17:21:54 +0100

    Facet-like implementation for multidropdown filter

--- dist/recline.extensions.js
+++ dist/recline.extensions.js
@@ -11305,6 +11305,7 @@ this.recline.View = this.recline.View || {};
             }
         
             self.buttonsData = {};
+            var alreadyInsertedValues = []
         	_.each(self._sourceDataset.getRecords(), function(record) {
                 var field = self._sourceDataset.fields.get(self.sourceField.field);
                 if(!field) {
@@ -11313,17 +11314,21 @@ this.recline.View = this.recline.View || {};
 
                 var fullLevelValue = record.getFieldValue(field);
                 var valueUnrendered = record.getFieldValueUnrendered(field);
-                if (self.separator && fullLevelValue.indexOf(self.separator) > 0)
+                if (!_.contains(alreadyInsertedValues, fullLevelValue))
             	{
-                	var levelValues = fullLevelValue.split(self.separator, 2);
-                	if (self.buttonsData[levelValues[0]] && self.buttonsData[levelValues[0]].options)
-                		self.buttonsData[levelValues[0]].options.push({fullValue: fullLevelValue, value: levelValues[1], record: record, selected: _.contains(self.sourceField.list, fullLevelValue)})
-                	else
-                		self.buttonsData[levelValues[0]] = { self: self, options: [{fullValue: fullLevelValue, value: levelValues[1], record: record, selected: _.contains(self.sourceField.list, fullLevelValue)}]}
-            	}
-                else
-            	{
-                	self.buttonsData[valueUnrendered] = { value: fullLevelValue, valueUnrendered: valueUnrendered, record: record, selected: _.contains(self.sourceField.list, valueUnrendered), self: self }
+                    if (self.separator && fullLevelValue.indexOf(self.separator) > 0)
+                	{
+                    	var levelValues = fullLevelValue.split(self.separator, 2);
+                    	if (self.buttonsData[levelValues[0]] && self.buttonsData[levelValues[0]].options)
+                    		self.buttonsData[levelValues[0]].options.push({fullValue: fullLevelValue, value: levelValues[1], record: record, selected: _.contains(self.sourceField.list, fullLevelValue)})
+                    	else
+                    		self.buttonsData[levelValues[0]] = { self: self, options: [{fullValue: fullLevelValue, value: levelValues[1], record: record, selected: _.contains(self.sourceField.list, fullLevelValue)}]}
+                	}
+                    else
+                	{
+                    	self.buttonsData[valueUnrendered] = { value: fullLevelValue, valueUnrendered: valueUnrendered, record: record, selected: _.contains(self.sourceField.list, valueUnrendered), self: self }
+                	}
+                    alreadyInsertedValues.push(fullLevelValue)
             	}
         	});
             
